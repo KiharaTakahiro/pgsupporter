d3cc69b0957e42f99fbe1a258b066128113ab4c1
KiharaTakahiro <takahirokihara123@gmail.com>
2022-08-20 17:17:42 +0900

    setupの設定を変更

--- setup.py
+++ setup.py
@@ -1,3 +1,19 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setup()
+requires = ["psycopg2>=2.8.6"]
+
+
+setup(
+    name='pgsupporter',
+    version='1.0.0',
+    description='pgsupporter',
+    url='https://github.com/KiharaTakahiro/pgsupporter/tree/main',
+    author='Takahiro Kihara',
+    author_email='takahirokihara123@gmail.com',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=requires,
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+    ],
+)
